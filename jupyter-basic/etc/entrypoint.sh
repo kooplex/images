e767b6e590c07a48659232ea8d57d7b6d65ab571
jegesm <david.visontai@complex.elte.hu>
2022-03-24 15:15:58 +0100

    fix jupyter rstudio

---
+++ jupyter-basic/etc/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+for SCRIPT in /.init_scripts/*
+do
+    if [ -x $SCRIPT ] ; then
+        echo "Running init script: $SCRIPT"
+	$SCRIPT
+    elif [ -f $SCRIPT ] ; then
+        echo "Sourcing init script: $SCRIPT"
+        . $SCRIPT
+    else
+        echo "Not a file $SCRIPT"
+    fi
+done
+
+echo "Sleeping for infinity"
+exec sleep infinity
